3b4e2b5eebc3c09bc66e3d8f1d56f47484c66eec
Lars Hadidi <lars.hadidi@gmail.com>
2024-12-19 22:18:43 +0000

    fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 def objective(n, a, l ,g, w, max_n, max_a):
-    return (- w[0] * n / max_n) + (w[1] * a / max_a) + w[2]*l + w[3]+g
+    return (- w[0] * n / max_n) + (w[1] * a / max_a) + w[2]*l + w[3]*g
 
 st.title("Optimal Estate")
 st.write('Editable source data')
@@ -14,11 +14,11 @@ max_area = df['Area'].max()
 
 st.latex(r'''-w_1\frac{n}{''' + str(max_neighbours) + r'''}+w_2\frac{a}{''' + str(max_area) + r'''} + w_3l + w_4g''')
 
-w = [1,1,1,1]
-w[0] = number = st.number_input("Neighbours Weight", min_value=0.0, max_value=1.0, value=1.0)
+w = [0,0,0,0]
+w[0] = number = st.number_input("Neighbours Weight", min_value=0.0, max_value=1.0, value=.0)
 w[1] = number = st.number_input("Area Weight", min_value=0.0, max_value=1.0, value=1.0)
-w[2] = number = st.number_input("Long Edge Weight", min_value=0.0, max_value=1.0, value=1.0)
-w[3] = number = st.number_input("South Garden Weight", min_value=0.0, max_value=1.0, value=1.0)
+w[2] = number = st.number_input("Long Edge Weight", min_value=0.0, max_value=1.0, value=.0)
+w[3] = number = st.number_input("South Garden Weight", min_value=0.0, max_value=1.0, value=.0)
 
 y = df.apply(lambda row: objective(row['Neighbours'], row['Area'], row['LongEdge'], row['SouthGarden'], w, max_neighbours, max_area), axis=1)
 
